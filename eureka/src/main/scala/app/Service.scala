d408861032c2021bab0b925c74c7c62c1cfe6e28
Alex Monad <degacth@yandex.ru>
2021-11-18 18:12:56 +0300

    add eureka server

--- eureka/src/main/scala/app/Service.scala
+++ eureka/src/main/scala/app/Service.scala
@@ -2,10 +2,11 @@ package app
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
-@EnableDiscoveryClient
-@SpringBootApplication class Service
+@EnableEurekaServer
+@SpringBootApplication
+class Service
 
 object Service extends App {
   SpringApplication.run(classOf[Service], args:_*)
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-gradle -p $APP_SERVICE_NAME bootRun
+GRADLE_OPTS=-Dorg.gradle.project.buildDir=/tmp/gradle-build gradle -p $APP_SERVICE_NAME bootRun
